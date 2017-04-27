6aecc8c02f0f18ece25d45728d9b7f47ff42cf47
musa1233 <musadiqbbr@gmail.com>
2017-04-27 07:03:20 -0400

    django

---
+++ advanceDjango/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/musadiq/Downloads/Django_By_Example_Code/Chapter_7/shop3env/bin/python3.4
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ advanceDjango/src/basic_app/migrations/0001_initial.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='School',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=256)),
+                ('principal', models.CharField(max_length=256)),
+                ('location', models.CharField(max_length=256)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Student',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=256)),
+                ('age', models.PositiveIntegerField()),
+                ('school', models.ForeignKey(to='basic_app.School', related_name='students')),
+            ],
+        ),
+    ]
